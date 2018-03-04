e07805db285dbab374efd86068a2f9ea1dd8cc7c
Oliver Richardson <orichardson95@gmail.com>
2018-03-04 08:15:06 +0000

    reshape images

--- train_all.sh
+++ train_all.sh
@@ -6,8 +6,10 @@ for ((i=0;i<NLABELS;i++)); do
 	printf "***********************************************************\n"
 	echo $"LABEL $i for $DATASET$"
 	SAMPLE_DIR=$"./samples/$DATASET-$i"
+	
+	SIZE = ${3:-28}
 
-	python main.py --dataset $1 --input_fname_pattern="$i/train/*.??g" --input_height=28 --output_height=28 --sample_dir=$SAMPLE_DIR --checkpoint_dir="./checkpoint/$1-$i" --epoch=20 --generate_test_images=300 --train
+	python main.py --dataset $1 --input_fname_pattern="$i/train/*.??g" --input_height=$3 --output_height=$3 --sample_dir=$SAMPLE_DIR --checkpoint_dir="./checkpoint/$1-$i" --epoch=20 --generate_test_images=300 --train
 	
 	python split.py $SAMPLE_DIR 64
 	
