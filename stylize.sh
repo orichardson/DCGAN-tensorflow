faf0b7ff421fb5eb1f5db30d5afb995d5a8c5674
Oliver Richardson <orichardson95@gmail.com>
2018-03-04 10:57:54 +0000

    styize with prints

--- stylize.sh
+++ stylize.sh
@@ -4,10 +4,12 @@ NLABELS=$3
 
 mkdir ../DGAN-tensorflow/samples/styl-$STYLE_NAME
 
-for ((i=0;i<NLABELS;i++)); do
+for ((i=0;i<NLABELS;i++)); 
+	echo $"******* Label $i ************"
 	mkdir ../DGAN-tensorflow/samples/styl-$STYLE_NAME/$i
+	echo $"made ../DGAN-tensorflow/samples/styl-$STYLE_NAME/$i"
 	
-	python evaluate.py --checkpoint style-models/$MODEL.ckpt \
+	python evaluate.py --checkpoint style-models/$MODEL.ckptgit  \
 		--in-path ../DCGAN-tensorflow/data/$DATASET/$i/train/ \
 		--out-path ../DCGAN-tensorflow/samples/styl-$STYLE_NAME/$DATASET-$i
 done
