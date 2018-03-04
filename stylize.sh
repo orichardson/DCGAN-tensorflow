19e5ad1eba13418c825fd10a684884d4a566cd0a
Oliver Richardson <orichardson95@gmail.com>
2018-03-04 10:55:58 +0000

    stylize works in general now

--- stylize.sh
+++ stylize.sh
@@ -1,14 +1,13 @@
-MODEL = $1
+STYLE_NAME= $1
 DATASET=$2
 NLABELS=$3
-STYLE_NAME= $4
 
 mkdir ../DGAN-tensorflow/samples/styl-$STYLE_NAME
 
 for ((i=0;i<NLABELS;i++)); do
 	mkdir ../DGAN-tensorflow/samples/styl-$STYLE_NAME/$i
 	
-	python evaluate.py --checkpoint $MODEL \
+	python evaluate.py --checkpoint style-models/$MODEL.ckpt \
 		--in-path ../DCGAN-tensorflow/data/$DATASET/$i/train/ \
 		--out-path ../DCGAN-tensorflow/samples/styl-$STYLE_NAME/$DATASET-$i
 done
