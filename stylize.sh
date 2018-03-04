6d32ff6b92cb02e21b1efd55a8e8da5ee213e727
Oliver Richardson <orichardson95@gmail.com>
2018-03-04 11:03:44 +0000

    styize with prints??

--- stylize.sh
+++ stylize.sh
@@ -4,12 +4,14 @@ NLABELS=$3
 
 mkdir ../DGAN-tensorflow/samples/styl-$STYLE_NAME
 
-for ((i=0;i<NLABELS;i++)); 
-	echo $"******* Label $i ************"
+for ((i=0;i<NLABELS;i++)); do 
+
+	printf $"******* Label $i ************"
 	mkdir ../DGAN-tensorflow/samples/styl-$STYLE_NAME/$i
-	echo $"made ../DGAN-tensorflow/samples/styl-$STYLE_NAME/$i"
+	echo made ../DGAN-tensorflow/samples/styl-$STYLE_NAME/$i
 	
 	python evaluate.py --checkpoint style-models/$MODEL.ckptgit  \
 		--in-path ../DCGAN-tensorflow/data/$DATASET/$i/train/ \
 		--out-path ../DCGAN-tensorflow/samples/styl-$STYLE_NAME/$DATASET-$i
+		
 done
