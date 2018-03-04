37a246ff31e5da8bb7bf731dbde757f31965d6be
Oliver Richardson <orichardson95@gmail.com>
2018-03-04 11:12:03 +0000

    styize more fixes #2

--- stylize.sh
+++ stylize.sh
@@ -9,10 +9,10 @@ for ((i=0;i<NLABELS;i++)); do
 	mkdir -p ../DCGAN-tensorflow/samples/styl-$STYLE_NAME/$i
 	echo made ../DCGAN-tensorflow/samples/styl-$STYLE_NAME/$i
 	
-	python evaluate.py --checkpoint style-models/$MODEL.ckpt \
+	python evaluate.py --checkpoint style-models/styl-$STYLE_NAME \
 		--in-path ../DCGAN-tensorflow/data/$DATASET/$i/train/ \
 		--out-path ../DCGAN-tensorflow/samples/styl-$STYLE_NAME/$DATASET-$i
-	echo evaluate.py --checkpoint style-models/$MODEL.ckpt \
+	echo evaluate.py --checkpoint style-models/styl-$STYLE_NAME \
 		--in-path ../DCGAN-tensorflow/data/$DATASET/$i/train/ \
 		--out-path ../DCGAN-tensorflow/samples/styl-$STYLE_NAME/$DATASET-$i
 		
