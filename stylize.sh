d28a252f1069be83ce0c1ae2f892baa4bbd3d9d3
Oliver Richardson <orichardson95@gmail.com>
2018-03-04 11:10:42 +0000

    styize more fixes

--- stylize.sh
+++ stylize.sh
@@ -2,15 +2,17 @@ STYLE_NAME=$1
 DATASET=$2
 NLABELS=$3
 
-mkdir ../DCGAN-tensorflow/samples/styl-$STYLE_NAME
 
 for ((i=0;i<NLABELS;i++)); do 
 
 	printf $"******* Label $i ************"
-	mkdir ../DCGAN-tensorflow/samples/styl-$STYLE_NAME/$i
+	mkdir -p ../DCGAN-tensorflow/samples/styl-$STYLE_NAME/$i
 	echo made ../DCGAN-tensorflow/samples/styl-$STYLE_NAME/$i
 	
-	python evaluate.py --checkpoint style-models/$MODEL.ckptgit  \
+	python evaluate.py --checkpoint style-models/$MODEL.ckpt \
+		--in-path ../DCGAN-tensorflow/data/$DATASET/$i/train/ \
+		--out-path ../DCGAN-tensorflow/samples/styl-$STYLE_NAME/$DATASET-$i
+	echo evaluate.py --checkpoint style-models/$MODEL.ckpt \
 		--in-path ../DCGAN-tensorflow/data/$DATASET/$i/train/ \
 		--out-path ../DCGAN-tensorflow/samples/styl-$STYLE_NAME/$DATASET-$i
 		
