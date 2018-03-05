a786706000fcd8afdf9039a6e06d0e31d24d2a06
Oliver Richardson <oer21@morganstanley.cl.cam.ac.uk>
2018-03-05 23:29:29 +0000

    new gitignore, fixed bug in multi-epoch, more results

--- multi-epoch-clf.sh
+++ multi-epoch-clf.sh
@@ -4,6 +4,6 @@ EPOCHS=${2-40}
 # cat  | python -i create-dataset-models.py
 for ((e=3;e<EPOCHS;e+=3)); do
 	echo for $e epochs...
-	python3 classifiers.py $DATASET DGAN --epoch=$e --log=cifar-dgan-epoch$e --models cnet
+	python3 classifiers.py $DATASET DGAN --epoch=$e --log=$DATASET-dgan-epoch$e --models cnet
 done
 
